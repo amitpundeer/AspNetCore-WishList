9dd60d93850f5782948ac55333735a4902c28c78
amitpundeer <amitpundeer039@gmail.com>
2018-07-05 12:33:29 +0530

    Configure EntityFramework

--- WishList/Data/ApplicationDbContext.cs
+++ WishList/Data/ApplicationDbContext.cs
@@ -9,6 +9,6 @@ namespace WishList.Data
     public class ApplicationDbContext :  DbContext
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
-        {}
+        { }
     }
 }
